55f2bc92043c4738962447fde9b0fb7b02a4370a
Andrew Walton <andrew.walton@rushcard.com>
2015-02-23 15:33:20 -0500

    Initial pass at ember-chosen component.

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 /* jshint node: true */
 'use strict';
 
+var pickFiles     = require('broccoli-static-compiler');
+var merge         = require('lodash.merge');
+
 module.exports = {
-  name: 'ember-cli-chosen'
+  name: 'ember-cli-chosen',
+  included: function(app) {
+    this._super.included(app);
+
+    // Setup default options for ember-cli-chosen
+    var options = merge({
+      'jQuery': true,
+      'importChosenCSS': true
+    }, app.options['ember-cli-chosen'] || {});
+
+    options.chosenJSType = options.jQuery ? 'jquery' : 'proto';
+
+    // Update `ember-cli-chosen` options on our `app` with updated hash
+    app.options['ember-cli-chosen'] = options;
+
+    // Import the correct JS for chosen
+    app.import(app.bowerDirectory + '/chosen/chosen.' + options.chosenJSType + '.js');
+
+    // Import Chosen CSS (done by default)
+    if(options.importChosenCSS) { app.import(app.bowerDirectory + '/chosen/chosen.css'); }
+  },
+  treeFor: function(treeName) {
+    var tree;
+
+    // Only include the Chosen sprites if we're including Chosen CSS in the build
+    if(treeName === 'public' && this.app.options['ember-cli-chosen'].importChosenCSS) {
+      tree = pickFiles(this.app.bowerDirectory + '/chosen', {
+        srcDir: '/',
+        files: ['*.png'],
+        destDir: '/assets'
+      });
+    }
+
+    return tree;
+  }
 };
