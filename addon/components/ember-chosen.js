01a190a2a25bb411329a656a7fac7f13edd62c8c
Kenan Klisura <kklisura@hotmail.com>
2015-05-09 08:51:26 +0200

    Initial core changes to chosen component

--- addon/components/ember-chosen.js
+++ addon/components/ember-chosen.js
@@ -1,79 +1,176 @@
 import Ember from 'ember';
 
+// We support these Chosen properties
+var CHOSEN_PROPERTIES = ['allowSingleDeselect',
+  'disableSearch',
+  'disableSearchThreshold',
+  'enableSplitWordSearch',
+  'inheritSelectClasses',
+  'maxSelectedOptions',
+  'noResultsText',
+  'placeholderTextMultiple',
+  'placeholderTextSingle',
+  'searchContains',
+  'singleBackstrokeDelete',
+  'width',
+  'displayDisabledOptions',
+  'displaySelectedOptions',
+  'includeGroupLabelInSelected'];
+
+// Custom handling for these properties
+var CUSTOM_PROPERTIES = ['isRtl', 'prompt', 'multiple'];
+
+// All suported properties
+var COMPONENT_PROPERTIES = CHOSEN_PROPERTIES.concat(CUSTOM_PROPERTIES);
+
 export default Ember.Component.extend({
   tagName: 'select',
-  attributeBindings: ['prompt:data-placeholder', 'multiple'],
   classNameBindings: ['isRtl:chosen-rtl'],
-  classNames: ['chosen-select'],
+  attributeBindings: ['prompt:data-placeholder', 'multiple'],
+
+  // select element specific properties 
   prompt: null,
-  isRtl: false,
+
   multiple: false,
-  disableSearchThreshold: null,
-  noResultsText: null,
-  maxSelectedOptions: null,
+
+  optionLabelPath: null,
+  optionValuePath: null,
+  optionGroupPath: null,
+
+  content: null,
+  selection: null,
   value: null,
-  width: '100%',
+
+  groupView: Ember.SelectOptgroup,
+  optionView: Ember.SelectOption,
+
+  isRtl: false,
+
+  // These actions are getting called
   selectionDidChange: null,
-  _options: function() {
-    var options = {};
+  chosenMaxSelected: null,
 
-    if(!Ember.isNone(this.get('disableSearchThreshold'))) { options['disable_search_threshold'] = this.get('disableSearchThreshold'); }
-    if(!Ember.isNone(this.get('noResultsText'))) { options['no_results_text'] = this.get('noResultsText'); }
-    if(!Ember.isNone(this.get('maxSelectedOptions'))) { options['max_selected_options'] = this.get('maxSelectedOptions'); }
-    if(!Ember.isNone(this.get('width'))) { options['width'] = this.get('width'); }
-
-    return options;
-  }.property('prompt', 'isRtl', 'multiple', 'disableSearchThreshold',
-             'noResultsText', 'maxSelectedOptions', 'width'),
-  _setupChosen: function() {
-    var _this = this,
-      options = _this.get('_options'),
-      isMultiple = _this.get('multiple'),
-      currentValue = _this.get('value'),
-      selectedValue;
-
-    if(isMultiple) {
-      currentValue = Ember.makeArray(currentValue);
-      _this.set('value', currentValue);
-    } else {
-      // If we're going from multiple -> single select, make the selected
-      // value the first item in the selected value array, if an item exists
-      if(Ember.isArray(currentValue) && currentValue.length > 0) {
-        currentValue = currentValue[0];
-        _this.set('value', currentValue);
-      }
-    }
-
-    _this.$().chosen(options)
-      .on('change', function (e, params) {
-      var index;
-
-      if(isMultiple) {
-        currentValue = _this.get('value');
-
-        if(params.selected) {
-          currentValue.pushObject(params.selected);
-        } else {
-          index = currentValue.indexOf(params.deselected);
-
-          if(index !== -1) {
-            currentValue.removeAt(index);
-          }
-        }
-
-        selectedValue = currentValue;
-      } else {
-        selectedValue = params.selected;
-      }
-
-      _this.set('value', selectedValue);
-      _this.sendAction('selectionDidChange', selectedValue);
-    }).on('chosen:maxselected', function(e, chosen) {
-      _this.sendAction('chosenMaxSelected', e, chosen);
-    });
-  }.observes('_options'),
   didInsertElement: function() {
     this._super();
     this._setupChosen();
+  },
+
+  groupedContent: function() {
+    var groupPath = this.get('optionGroupPath');
+    var groupedContent = Ember.A();
+    var content = this.get('content') || [];
+
+    content.forEach(function(item) {
+      var label = item[groupPath];
+
+      if (groupedContent.get('lastObject.label') !== label) {
+        groupedContent.pushObject({
+          label: label,
+          content: Ember.A()
+        });
+      }
+
+      groupedContent.get('lastObject.content').push(item);
+    });
+
+    return groupedContent;
+  }.property('optionGroupPath', 'content.@each'),
+
+  // Chosen specific options
+  _options: Ember.computed(COMPONENT_PROPERTIES, function () {
+    var options = {};
+
+    CHOSEN_PROPERTIES.forEach(function (propertyName) {
+      var propertyValue = this.get(propertyName);
+      if (!Ember.isNone(propertyValue)) {
+        options[propertyName.decamelize()] = propertyValue;
+      }
+    }.bind(this));
+
+    return options;
+  }),
+
+  _getFirstContent: function () {
+    var optionValuePath = this.get('optionValuePath'),
+      content = this.get('content'),
+      value = null,
+      match;
+
+    if (Ember.isNone(content) || !Ember.isArray(content) || Ember.isEmpty(content)) {
+      return null;
+    }
+
+    if (optionValuePath) {
+      match = optionValuePath.match(/content\.(.+)/);
+      if (match) {
+        value = content[0][match[1]];
+      } else {
+        Ember.Logger.error('optionValuePath \'' + optionValuePath + '\' does not reference \'content\'');
+      }
+    } else {
+      value = content[0];
+    }
+
+    return value;
+  },
+
+  _setupChosen: function() {
+    var options = this.get('_options'),
+      isMultiple = this.get('multiple'),
+      currentSelection = this.get('selection'),
+      currentValue = this.get('value');
+
+    // Set initial selection to value and selection
+    if (isMultiple) {
+      currentSelection = Ember.makeArray(currentSelection);
+      this.set('selection', currentSelection);
+    } else {
+      if (Ember.isNone(currentValue)) {
+        if (Ember.isNone(currentSelection)) {
+          // If no value or selection supplied, use first value from content
+          currentValue = this._getFirstContent();
+        } else {
+          if (Ember.isArray(currentSelection)) {
+            currentValue = currentSelection[0];
+          } else {
+            currentSelection = currentSelection;
+          }
+        }
+      }
+      this.set('selection', currentValue);
+      this.set('value', currentValue);
+    }
+
+    // Subscribe to chosen events
+    this.$().chosen(options)
+      .on('change', this._selectionChanged.bind(this))
+      .on('chosen:maxselected', this.sendAction.bind(this, 'chosenMaxSelected'));
+
+  }.observes('_options'),
+
+  _selectionChanged: function (ev, params) {
+    var selectedValue = params.selected,
+      isMultiple = this.get('multiple'),
+      currentSelection,
+      index;
+
+    if (isMultiple) {
+      currentSelection = this.get('selection');
+
+      if (params.selected) {
+        currentSelection.pushObject(selectedValue);
+      } else {
+        index = currentSelection.indexOf(params.deselected);
+
+        if (index !== -1) {
+          currentSelection.removeAt(index);
+        }
+      }
+    } else {
+      this.set('selection', selectedValue);
+      this.set('value', selectedValue);
+    }
+
+    this.sendAction('selectionDidChange', selectedValue);
   }
 });
