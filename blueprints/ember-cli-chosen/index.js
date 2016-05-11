2cc63d7d69a99c1444a6683e7d587a0b47e87816
Andrew Walton <andrew@greenarrow.me>
2016-05-10 20:26:14 -0400

    Fixing issue with bower package resolution in blueprint

--- blueprints/ember-cli-chosen/index.js
+++ blueprints/ember-cli-chosen/index.js
@@ -3,8 +3,6 @@ module.exports = {
   description: 'Include "Chosen" bower package',
 
   afterInstall: function(options) {
-    return this.addBowerPackagesToProject([
-      { name: 'chosen=https://github.com/harvesthq/chosen/releases/download/v1.3.0/chosen_v1.3.0.zip' }
-    ]);
+    return this.addBowerPackageToProject('chosen', 'https://github.com/harvesthq/chosen/releases/download/v1.3.0/chosen_v1.3.0.zip');
   }
 };
